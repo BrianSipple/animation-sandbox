22441e6ce86c74baba0bf008b2aaaa41151c53a4
BrianSipple <bsipple57@gmail.com>
2015-07-04 03:24:57 -0700

    initial commit of Samuel L. GSAP

---
+++ views/samuel-l-gsap/REAME.md
@@ -0,0 +1,12 @@
+# Samuel L. GSAP
+
+[What?](../../assets/img/say-what-again.jpg)
+
+Samuel L. GSAP is a demo that uses GSAP to toggle a ticker of text.
+
+The source of that text comes courtesy of reference by [ipsums.com]() -- the greatest place in the world for one to go
+whenever they need large blocks of sample text. As you might have guessed, there's a (Samuel L Jackson generator).
+
+**Disclaimer** Much of the aforementioned site's content happens to be... very unsafe for all sensibilites. 
+The texts used here have been chosen with discretion, but aren't guaranteed to be 100% mofo free. 
+
---
+++ views/samuel-l-gsap/samuel-l-gsap.js
@@ -0,0 +1,158 @@
+(function (window) {
+
+    var
+        mainContentContainer = document.querySelector('.content'),
+        slidingTextContainer = mainContentContainer.querySelector('.text-container'),
+        slidingTextElem = slidingTextContainer.querySelector('.text'),
+        animationToggleButton = mainContentContainer.querySelector('.button--timeline-toggle'),
+
+        slipsums = [],// Samuel L.-Ipsums... parsed out of our JSON on-load
+        numSlipsums,
+        NETWORK_TIMEOUT_THRESHOLD = 7500, // max amount of time for network requests before throwing an error
+        slipsumsPath = '../../assets/data/slipsums.json',
+
+        isTextAnimating = false,
+
+        ANIMATION_DURATION_MULTIPLIER = 1,
+        DURATIONS = {
+            slideText: ANIMATION_DURATION_MULTIPLIER * 6
+        },
+
+        slideTextTlOpts = {
+            repeat: 0,
+            onComplete: generateMoreText,
+            onCompleteParams: ['{self}']
+        },
+
+        masterTL = new TimelineMax();
+
+
+    TweenMax.set(
+        mainContentContainer,
+        {
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            xPercent: -50,
+            yPercent: -50
+        }
+    );
+
+
+
+    function boundedRandom(min, max) {
+        if (!max) {
+            max = min;
+            min = 0;
+        }
+        return Math.floor(min + ( Math.random() * (max - min) ));
+    }
+
+
+    /**
+     * Choose a random slipSum and fill up our sliding text element
+     */
+    function generateMoreText(tl) {
+
+        TweenMax.set(slidingTextElem, {x: '100%'});
+        var
+            idx = boundedRandom(numSlipsums),
+            slipSum = slipsums[idx];
+
+        //slidingTextElem.textContent += slipSum;   // QUESTION: Better to just keep adding it to the end? I'm thinking so both perfwise and for the appearance of the rendered text
+        slidingTextElem.textContent = slipSum;
+        tl.add(triggerSlide(DURATIONS.slideText));
+
+        function triggerSlide(duration) {
+            debugger;
+            var tween = TweenMax.to(slidingTextElem, duration, {x: '-100%', ease: Linear.easeNone});
+            return tween;
+        }
+    }
+
+
+    function resumeSlide() {
+        masterTL.resume();
+
+    }
+
+    function pauseSlide() {
+        masterTL.pause();
+    }
+
+
+    function parseJSON(path) {
+        return new Promise(function (resolve, reject) {
+
+            var networkTimeout = setTimeout(function () {
+                throw new Error('The request has been timed out with great vengeance.');
+            }, NETWORK_TIMEOUT_THRESHOLD);
+
+            var req = new XMLHttpRequest();
+            req.open('GET', path);
+
+            req.onload = function () {
+                // onload is called even on a 404, so check the status
+                if (req.status === 200) {
+                    clearTimeout(networkTimeout);
+                    resolve(req.response);
+                }
+                else {
+                    reject(Error(req.statusText));
+                }
+            };
+
+            // everybody strap in!
+            req.send();
+        });
+    }
+
+    function getSlipsums() {
+
+        return parseJSON(slipsumsPath)
+            .then(function (resp) {
+
+                var texts = JSON.parse(resp);
+                //debugger;
+
+                numSlipsums = texts.slipsums.length;
+                for (var i = 0; i < numSlipsums; i++) {
+                    slipsums.push(texts.slipsums[i].text);
+                }
+            })
+            .catch(function (err) {
+                console.log('What\'s this bitch-ass JSON error for? -- fix it, motherfucker: ' + err);
+            });
+    }
+
+
+    function initSlideTextTL () {
+        var tl = new TimelineMax(slideTextTlOpts);
+        generateMoreText(tl);
+        return tl;
+    }
+
+
+    function init() {
+
+        animationToggleButton.disabled = true;  // lock until we load
+
+        getSlipsums().then(function () {
+            masterTL.add(initSlideTextTL());
+            animationToggleButton.disabled = false;
+        });
+
+        animationToggleButton.addEventListener('mouseup', function () {
+
+            isTextAnimating ? resumeSlide() : pauseSlide();
+            isTextAnimating = !isTextAnimating;
+
+        }, false);
+    }
+
+    window.addEventListener('load', function () {
+        init();
+
+    }, false);
+
+}(window));

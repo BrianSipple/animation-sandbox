ff24013db235d8d442bba92b9dab688651188bdf
BrianSipple <bsipple57@gmail.com>
2015-07-06 12:59:01 -0700

    leaf drawn. TODO: Bezier pathing and physics!

--- views/svg-lab/falling-leaf/falling-leaf.js
+++ views/svg-lab/falling-leaf/falling-leaf.js
@@ -3,46 +3,48 @@
     var leafContainer = document.querySelector('.svg-container'),
         leafSVG = leafContainer.querySelector('#leafSVG'),
 
-        ANIMATION_DURATION_MULTIPLIER = 1,
-        masterTl = new TimelineMax();
+        ANIMATION_DURATION_MULTIPLIER = 1;
 
-    debugger;
+    //debugger;
     TweenMax.set(
         leafContainer,
         {
             position: 'absolute',
             right: 0,
-            top: '10%'
+            top: '10%',
+            transformOrigin: '50% 50%'
         }
     );
 
 
     function fall() {
 
+        var fallTl = new TimelineMax();
+
         // Setup the paths of each "flutter" (a path of motion before a directional
         // change occurs due to the lift....
         var fallPath = {
-                flutter1: [
-                    {x: '100%', y: '+=0'},
-                    {x: '75%', y: '10%'},
-                    {x: '75%', y: '-10%'}
-                ]
-            },
+            flutter1: [
+                {left: '-=0', top: '-=0', rotationY: 0, rotationX: 0, rotationZ: 0},
+                {left: '-=25%', top: '-=10%', rotationY: 0, rotationX: '-=20%', rotationZ: '-=20%'},
+                {left: '-=25%', top: '+=20%', rotationY: 0, rotationX: '+=40%', rotationZ: '+=40%'}
+            ],
+            flutter2: []
+        };
 
-            tween = TweenMax.to(
+        fallTl.add(
+            TweenMax.to(
                 leafContainer,
-                ANIMATION_DURATION_MULTIPLIER,
+                ANIMATION_DURATION_MULTIPLIER * 3,
                 {
                     bezier: {
                         values: fallPath.flutter1
                     },
                     ease: Power3.easeOut
                 }
-            );
+            )
+        );
 
-        debugger;
-
-        masterTl.add(tween);
 
     }
 

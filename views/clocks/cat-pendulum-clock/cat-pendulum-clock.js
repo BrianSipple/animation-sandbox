fcefaef76894de7240fb7c4183e1a512f2fac3e9
BrianSipple <bsipple57@gmail.com>
2015-08-18 22:24:01 -0700

    proper physics have been computed -- we just need to tighten up positioning

--- views/clocks/cat-pendulum-clock/cat-pendulum-clock.js
+++ views/clocks/cat-pendulum-clock/cat-pendulum-clock.js
@@ -7,8 +7,12 @@ var app = (function (exports) {
             clockSVG: '#Clock',
             clockBodySVG: '#ClockBody',
             tailSVG: '#Tail',
+            leftEyeSVG: '#EyeLeft',
             leftEyelidSVG: '#EyelidLeft',
+            rightEyeSVG: '#EyeRight',
             rightEyelidSVG: '#EyelidRight',
+            hourHandSVG: '#HourHand',
+            minuteHandSVG: '#MinuteHand',
             hourHandBaseSVG: '#HourHandBase',
             minuteHandBaseSVG: '#MinuteHandBase'
         },
@@ -16,10 +20,15 @@ var app = (function (exports) {
         clockSVG = document.querySelector(SELECTORS.clockSVG),
         clockBodySVG = clockSVG.querySelector(SELECTORS.clockBodySVG),
         tailSVG = clockSVG.querySelector(SELECTORS.tailSVG),
+        leftEyeSVG = clockSVG.querySelector(SELECTORS.leftEyeSVG),
         leftEyelidSVG = clockSVG.querySelector(SELECTORS.leftEyelidSVG),
+        rightEyeSVG = clockSVG.querySelector(SELECTORS.rightEyeSVG),
         rightEyelidSVG = clockSVG.querySelector(SELECTORS.rightEyelidSVG),
+        hourHandSVG = clockSVG.querySelector(SELECTORS.hourHandSVG),
         hourHandBaseSVG = clockSVG.querySelector(SELECTORS.hourHandBaseSVG),
+        minuteHandSVG = clockSVG.querySelector(SELECTORS.minuteHandSVG),
         minuteHandBaseSVG = clockSVG.querySelector(SELECTORS.minuteHandBaseSVG),
+        
                     
         
         // Use to prevent ugly timing behavior
@@ -32,7 +41,6 @@ var app = (function (exports) {
         previousTime = new Date(),
         currentTime,
         elapsedTimeMS,
-        deltaT,         // elapsed time normalized to seconds
         
         // Pendulum
         newPerpendicularForceOnPendulum,
@@ -41,55 +49,36 @@ var app = (function (exports) {
         
         
         //////////////////////// Models ////////////////////////
-        
-        
-//        /**
-//         * We'll model the clock pendulum as a point-mass at the end of a weightless rod (i.e: no friction in the equation)
-//         */
-//        Pendulum = {
-//            el: undefined,
-//            mass: 1,
-//            length: 0,
-//            swingSpan: 0,
-//            startingLeftX: 0,
-//            currentLeftX: 0,
-//            
-//            /* angle of rotation (theta 0 == pointing horizontally to the right) */
-//            theta: Math.PI / 2 - 0.05,
-//                
-//            /* Angular velocity (radians per second) */
-//            omega: 0,
-//            
-//            /* Angular acceleration (radians per second^2) */
-//            alpha: 0,
-//            
-//            /* Moment of Inertia (Resistance to change in rotation (kg / m^2)) */
-//            J: 0                        
-//        },
-        
+                    
         PendulumObjectProto = {
             el: undefined,
-            startingLeftX: 0,
-            currentLeftX: 0,
-            mass: 1,
+            pos: {
+                cx: 0
+            },
+            mass: 1,    
             length: 0,
             width: 0,
-            theta: Math.PI / 2,
+            theta: 0, // 0 ===> Horizontal and pointing to the right. Math.PI / 2 ==> pointing straight down,
             omega: 0,
             alpha: 0,
             J: 0,
             
-            initFromSVG: function initFromSVG (svgElem) {
+            initFromSVGs: function initFromSVGs (svgElem, containerSVGElem) {
+                debugger;
                 
-                var elBox = svgElem.getBBox();
+                var 
+                    boundingRect = svgElem.getBoundingClientRect(),
+                    containerBoundingRect = containerSVGElem.getBoundingClientRect();
         
                 this.el = svgElem;
-                this.length = elBox.width;
-                this.width = elBox.height;
-                this.startingLeftX = this.currentLeftX = elBox.x;
-                this.swingSpan = ( this.length / Math.tan(Math.PI/2) ) * 2;
-                //this.J = this.mass * (this.length * this.length);
-                this.J = this.mass * (this.length * this.length / this.length);
+                this.length = boundingRect.height;
+                this.width = boundingRect.width;
+                //this.startingLeftX = this.currentLeftX = boundingRect.left;
+                this.pos.cx = boundingRect.left + ( (boundingRect.right - boundingRect.left) / 2);
+                
+                this.swingSpan = containerBoundingRect.width;
+                this.J = this.mass * (this.length * this.length);  // moment of inertia
+                //this.J = this.mass * (this.length * this.length / this.length);
             },
             
             /**
@@ -98,8 +87,9 @@ var app = (function (exports) {
              * BOTH calculated relative to time.
              */
             updateState: function updateState (elapsedTimeMS) {
+                debugger;
                 var deltaT = elapsedTimeMS / 1000;  // MS to seconds
-                preventCrazyTimeWarping();
+                deltaT = adjustDeltaToHandleCrazyTimeWarping(deltaT);
 
                 this.theta += this.omega * deltaT + (0.5 * this.alpha * deltaT * deltaT);
 
@@ -110,25 +100,66 @@ var app = (function (exports) {
                  * taking the [mass] * [gravity due to Earth (9.81 m/s^2)] * [length]
                  * 2) Torque is the perpendicular force multiplied by distance from center of rotation
                  */
-                newPerpendicularForceOnPendulum = this.mass * 9.81 * Math.cos(this.theta);
+                newPerpendicularForceOnPendulum = this.mass * 9.81 * Math.cos(this.theta) * this.length;
                 newPendulumTorque = newPerpendicularForceOnPendulum * this.length;
 
                 newPendulumAcceleration = newPendulumTorque / this.J;
 
                 /* Calculate current angular velocity from last frame's ang. velocity and 
                 average of last frame's acceleration with this frame's acceleration. */
-                this.omega += 0.5 * (newPendulumAcceleration + this.alpha);
+                this.omega += 0.5 * (newPendulumAcceleration + this.alpha) * deltaT;
 
                 /* Update acceleration */
                 this.alpha = newPendulumAcceleration;  
+                
+//                console.log('Elapsed Time: ' + elapsedTimeMS);
+//                console.log('DeltaT: ' + deltaT);
+//                console.log('Theta: ' + this.theta);
+//                console.log('Omega: ' + this.omega);
+//                console.log('Alpha: ' + this.alpha);
+//                console.log('Torque: ' + newPendulumTorque);
             },
             
             animate: function animate () {
-                this.currentLeftX += (this.swingSpan / 2) + (this.length * Math.cos(this.theta));
-
+                debugger;
+                //this.currentLeftX += (this.swingSpan / 2) + (this.length * Math.cos(this.theta));
+                //this.currentLeftX += this.length * Math.cos(this.theta);
+                //this.currentLeftX = this.startingLeftX + ( ( Math.cos(this.theta) * (this.swingSpan/2) ) - this.width);
+                
+                var 
+                    newX = this.pos.cx + (this.length * Math.cos(this.theta)),
+                    xIncrement = newX - this.pos.cx;                        
+                
+                console.log('Animated el: ' + this.el);
+                console.log('Previous CX: ' + this.pos.cx);
+                console.log('New CX: ' + newX);
+                console.log('Computed Rotation: ' + this.theta);
+                console.log('Max Angle: ' + this.maxTheta);
+                
+                // update the state -- then set it on the element
+                this.pos.cx = newX;
+                
+                // Subtract an extra PI/2 to account for the fact that we start in the downward (PI/2 rad) postion 
+                var rotation = (this.theta - (Math.PI / 2)) / 2;
+                
+                
+                
+                if (this.maxTheta) {
+                    if (this.theta > this.maxTheta) {
+                        rotation = ( rotation / (Math.PI/4) ) * this.maxTheta;
+                        console.log('New Rotation: ' + rotation);
+                    
+                    } else if (this.theta < -this.maxTheta) {
+                        rotation = ( rotation / (Math.PI/4) ) * -this.maxTheta;
+                        console.log('New Rotation: ' + rotation);
+                    }
+                }
+                
+                
                 this.el.style.transform = 
-                    'translateX(' + (this.startingLeftX - this.currentLeftX) + 'px) ' +
-                    'rotateZ(' + this.theta + 'rad)';
+                    'translateX(' + xIncrement + 'px) ' +
+                    //'rotateZ(' + this.theta + 'rad)';
+                    'rotateZ(' + rotation + 'rad)';
             }        
         },
                         
@@ -139,46 +170,64 @@ var app = (function (exports) {
         
         ClockHandProto = {
             el: undefined,
-            diameter: 0,
-            theta: Math.PI / 2 // (Math.PI / 2 == noon, 0 == 3:00)
+            clockBaseEl: undefined,
+            theta: Math.PI / 2, // (Math.PI / 2 == noon, 0 == 3:00)
+            
+            initFromSVGs: function initFromSVGs (mainSVG, clockBaseSVG) {
+              
+                this.el = mainSVG;
+                this.clockBaseEl = clockBaseSVG;
+                
+                var yTransOriginPercentage = 
+                    100 - ( Number(this.clockBaseEl.getAttribute('r')) / this.el.getBBox().height );
+                            
+                this.el.style.transformOrigin = ('50% ' + yTransOriginPercentage + '%');
+            },
+            
+            animate: function animate () {
+                debugger;
+                this.el.style.transform = 'rotateZ(' + this.theta + 'rad)';
+            }
+            
         },
         
-        HourHandBase = Object.create(ClockHandProto),    
-        MinuteHandBase = Object.create(ClockHandProto);
+        HourHand = Object.create(ClockHandProto),    
+        MinuteHand = Object.create(ClockHandProto);
 
         
     
-    function getHoursTheta(numHours) {
-        return Math.PI * 2 * ( ( numHours % 12) / 12 );
-    }
-    
-    function getMinutesTheta(numMinutes) {
-        return (Math.PI * 2 / 12) * ( (numMinutes % 60) / 60);
-    }
-    
-    
-    function computeClockHandThetas (time) {
+    function computeHourHandTheta (time) {
+        
         var 
             hours = time.getHours(),
-            mins = time.getMinutes();
+            minutes = time.getMinutes(),
+            seconds = time.getSeconds();
         
-        HourHandBase.theta = 
-            getHoursTheta(hours) + 
-            getMinutesTheta(mins) -
-            Math.PI / 2; 
-        
-        MinuteHandBase.theta = getMinutesTheta(mins);  
+        return ( (hours % 12) + (minutes / 60) + (seconds / 3600) ) 
+            * 30 
+            * (Math.PI / 180) 
+            - Math.PI / 2;
     }
     
-    function initClockHands () {        
-        HourHandBase.el = hourHandBaseSVG;
-        MinuteHandBase.el = minuteHandBaseSVG;        
-        computeClockHandThetas(previousTime);          
+    function computeMinuteHandTheta (time) {
+        
+        var 
+            minutes = time.getMinutes(),
+            seconds = time.getSeconds();
+        
+        return ( ((minutes / 60 * 12) % 12) + (seconds / 60) )
+            * 30
+            * (Math.PI / 180)
+            - Math.PI / 2;            
     }
     
+    function computeClockHandThetas (time) {
         
+        HourHand.theta = computeHourHandTheta(time);         
+        MinuteHand.theta = computeMinuteHandTheta(time);
+    }
+    
     function performPendulumAnimations () {
-        debugger;                   
         [
             Pendulum,
             LeftEyelid,
@@ -189,14 +238,13 @@ var app = (function (exports) {
         });
     }
     
-    function rotateClockHands () {
-        debugger;
+    function rotateClockHands () {        
         [
-            HourHandBase,
-            MinuteHandBase
+            HourHand,
+            MinuteHand
         ]
-        .forEach(function (handBase) {   
-            handBase.el.style.transform = 'rotateZ(' + handBase.theta + 'rad)';            
+        .forEach(function (hand) { 
+            hand.animate();            
         });
     }
     
@@ -215,9 +263,11 @@ var app = (function (exports) {
      *    To ensure that the universe remains in alignment 
      *    we'll bound deltaT to 50ms.
      */
-    function preventCrazyTimeWarping() {        
-        if (deltaT > MAX_FRAME_DELTA) {
-            deltaT = MAX_FRAME_DELTA;
+    function adjustDeltaToHandleCrazyTimeWarping(currentDelta) {        
+        if (currentDelta > MAX_FRAME_DELTA) {
+            return MAX_FRAME_DELTA;
+        } else {
+            return currentDelta;
         }
     }
     
@@ -234,17 +284,37 @@ var app = (function (exports) {
     }
     
     function initPendulumObjects () {
-        Pendulum.initFromSVG(tailSVG);
-        LeftEyelid.initFromSVG(leftEyelidSVG);
-        RightEyelid.initFromSVG(rightEyelidSVG);
+        Pendulum.initFromSVGs(tailSVG, clockBodySVG);
+        LeftEyelid.initFromSVGs(leftEyelidSVG, leftEyeSVG);
+        RightEyelid.initFromSVGs(rightEyelidSVG, rightEyeSVG);
     }
+    
+    function initClockHands () { 
+        HourHand.initFromSVGs(hourHandSVG, hourHandBaseSVG);
+        MinuteHand.initFromSVGs(minuteHandSVG, minuteHandBaseSVG);        
+    }
+    
+    
+    /**
+     * We want to apply pendulum-swing physics to the horizontal sweeping
+     * motion of the eyelid, but restrict its angle to a tighter span
+     * due to the nature of its position / alignment within the eyeball
+     */
+    function restrictEyelidTheta (thetaLimit) {
+        RightEyelid.maxTheta = thetaLimit;
+        LeftEyelid.maxTheta = thetaLimit;
+    }
+    
             
     function runTheClock () {
                 
         requestAnimationFrame(runTheClock);
         
+        debugger;
+        
         currentTime = new Date();
         elapsedTimeMS = currentTime.getTime() - previousTime.getTime();  // elapsed time in ms b/w frames
+        previousTime = currentTime;
         
         computePendulumStates(elapsedTimeMS);
         computeClockHandThetas(currentTime);
@@ -254,8 +324,9 @@ var app = (function (exports) {
     
     function init () {                                
         initPendulumObjects();        
+        restrictEyelidTheta(Math.PI/10);
         initClockHands();
-
+        computeClockHandThetas(previousTime);
         runTheClock();
     }
     

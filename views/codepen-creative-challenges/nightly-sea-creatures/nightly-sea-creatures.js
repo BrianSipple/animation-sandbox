6714ca4805dd4f85a47e94ccc9f0b127d7c9da36
BrianSipple <bsipple57@gmail.com>
2015-08-21 23:30:13 -0700

    more files setup

---
+++ views/codepen-creative-challenges/nightly-sea-creatures/nightly-sea-creatures.js
@@ -0,0 +1,16 @@
+var SeaCreatures = (function (exports) {
+    
+    'use strict';
+    
+    function init () {
+        
+    }
+    
+    return {
+        init: init 
+    };
+    
+}(window));
+
+
+window.addEventListener('DOMContentLoaded', SeaCreatures.init, false);

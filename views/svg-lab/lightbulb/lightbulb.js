669c7afd61a53553c24d019f70036d85d53892cc
BrianSipple <bsipple57@gmail.com>
2015-08-24 22:28:24 -0700

    Initial setup

--- views/svg-lab/lightbulb/lightbulb.js
+++ views/svg-lab/lightbulb/lightbulb.js
@@ -12,41 +12,115 @@ let lightbulb = function lightbulb () {
             SELECTORS = {
                 mainSVGContainer: '.svg-container',
                 mainLightbulb: '#lightbulb',
-                bulb: '#bulb',
-                lightbulbGlow: '#bulbGlowLayer',
+                bulbGlass: '#bulbGlass',
+                bulbInnerCharging: '#bulbInnerCharging',
+                bulbInnerLit: '#bulbInnerLit',
+                bulbGlowFilter: 'filter#surroundingBlur',
+                bulbGlowFilterBlurNode: '.blur-node',
+                wireTurbulenceFilter: 'filter#turbulence',
+                wireTurbulenceNode: '.turbulence-node',
                 energyWireUnlit: '#energyWireUnlit',
                 energyWireLit: '#energyWireLit'            
             },
 
             EASINGS = {
-                default: Power4.easeInOut  
+                default: Power4.easeInOut,
+                linear: Power0.easeNone
             },
 
             LABELS = {
                 phaseWireIsCharging: 'phase__wire-is-charging',
-                phaseFullChargeReached: 'phase__full-charge-reached',
+                phaseFlickeringToFullLight: 'phase__flickering-to-full-light',
                 phaseBulbOnAndFlickering: 'phase__bulb-on-and-flickering',
-                phaseWireIsCharging: 'phase__wire-is-charging',
+                
+                wireChargeStart: 'wire-charge-is-starting',
+                
+                chargeFlicker1: 'charge-flicker-1',
+                chargeFlicker2: 'charge-flicker-2',
+                chargeFlicker3: 'charge-flicker-3',
+                chargeFlicker4: 'charge-flicker-4',
+                chargeFlicker5: 'charge-flicker-5',
+                chargeFlicker6: 'charge-flicker-6',
+                
+                chargeFlicker1Complete: 'charge-flicker-1-complete',
+                chargeFlicker2Complete: 'charge-flicker-2-complete',
+                chargeFlicker3Complete: 'charge-flicker-3-complete',
+                chargeFlicker4Complete: 'charge-flicker-4-complete',
+                chargeFlicker5Complete: 'charge-flicker-5-complete',
+                chargeFlicker6Complete: 'charge-flicker-6-complete'
             },
 
             DURATIONS = {
-                wireCharging: 1.2
+                initialWireCharging: 1.2,
+                chargeFlicker1: 1.2,
+                chargeFlicker2: 0.7,
+                chargeFlicker3: 1.7,
+                chargeFlicker4: 0.3,
+                chargeFlicker5: 0.9,
+                chargeFlicker6: 1.2
             },
 
             COLORS = {
-                wireCharging: '#E68A3A'
+                chargingOrange: '#E68A3A',
+                litYellow: '#FFF3AA'
             },
+            
+            MAX_GLOW_FILTER_SD = 90,
             
             // DOM Refs,
             mainSVGContainer,
             mainLightbulbSVG,
-            lightbulbGlowLayer,
-            bulbSVG,
+            bulbGlowFilter,
+            bulbGlowFilterBlurNode,
+            bulbGlassSVG,
+            bulbInnerChargingSVG,
+            bulbInnerLitSVG,             
             energyWireUnlitSVG,
             energyWireLitSVG,
             
 
             masterTL,
+            
+            currentUniqueLabelNum = 1,
+            getUniqueLabel = () => {
+                return 'unique-label-' + currentUniqueLabelNum++;
+            },
+            
+            /**
+             * Helper to set the amount of ligthing in the bulb
+             */
+            setBulbLighting = (tl, elem, intensityFactor, hexColor, duration, label = getUniqueLabel()) => {
+                  
+                //debugger;
+                let 
+                    bulbTween = TweenMax.to(
+                        elem,
+                        duration,
+                        {
+                            autoAlpha: intensityFactor,
+                            ease: EASINGS.default, // TODO: Create a good easing function
+                            attr: {                            
+                                fill: hexColor                            
+                            }
+                        }
+                    ),
+                    
+                    glowTween = TweenMax.to(
+                        bulbGlowFilterBlurNode, 
+                        duration, 
+                        { 
+                            attr: { 
+                                stdDeviation: (MAX_GLOW_FILTER_SD * intensityFactor ) 
+                            },
+                            ease: EASINGS.default
+                        }
+                    );
+                
+                
+                tl.add(bulbTween, label);
+                tl.add(glowTween, label);
+            },
+            
 
             
             setScene = () => {
@@ -56,55 +130,52 @@ let lightbulb = function lightbulb () {
                 // cache DOM refs
                 mainSVGContainer = document.querySelector(SELECTORS.mainSVGContainer),
                 mainLightbulbSVG = mainSVGContainer.querySelector(SELECTORS.mainLightbulb),
-                lightbulbGlowLayer = mainSVGContainer.querySelector(SELECTORS.lightbulbGlow),
-                bulbSVG = mainLightbulbSVG.querySelector(SELECTORS.bulb),
+                bulbGlowFilter = document.querySelector(SELECTORS.bulbGlowFilter),
+                bulbGlowFilterBlurNode = bulbGlowFilter.querySelector(SELECTORS.bulbGlowFilterBlurNode), 
+                bulbGlassSVG = mainLightbulbSVG.querySelector(SELECTORS.bulbGlass),
+                bulbInnerChargingSVG = mainLightbulbSVG.querySelector(SELECTORS.bulbInnerCharging),
+                bulbInnerLitSVG = mainLightbulbSVG.querySelector(SELECTORS.bulbInnerLit),
                 energyWireUnlitSVG = mainLightbulbSVG.querySelector(SELECTORS.energyWireUnlit),
                 energyWireLitSVG = mainLightbulbSVG.querySelector(SELECTORS.energyWireLit);  
                 
-                sceneSetTL.set(lightbulbGlowLayer, { autoAlpha: 0 });
-                sceneSetTL.set(energyWireUnlitSVG, { drawSVG: false });
+                // blur out and undraw the second, lit wire
+                sceneSetTL.set(energyWireLitSVG, { drawSVG: false });
+                
+                // blur out glow layer and inner layers
+                sceneSetTL.set(bulbGlowFilterBlurNode, { attr: {stdDeviation: 0} });
+                
+                // Blur out, and set initial scaling and transform origin for the inner layers
                 sceneSetTL.set(
-                    bulbSVG, 
+                    [
+                        bulbInnerChargingSVG,
+                        bulbInnerLitSVG                     
+                    ],
                     { 
                         scale: 0, 
                         transformOrigin: '50% 50%', 
-                        autoAlpha: 0, 
-                        fill: COLORS.wireCharging 
+                        autoAlpha: 0                        
                     }
                 );
+                
+                // We should be all set -- remove opacity from the main SVG!
+                // NOTE: We manually set opacity and visibility here, 
+                // because autoAlpha: 1 would set visbility to inherit
+                sceneSetTL.set(mainSVGContainer, { opacity: 1, visibility: 'visible' });
                 
                 return sceneSetTL;
             },
             
             
-            /**
-             * Helper to set the amount of ligthing in the bulb
-             */
-            setBulbLighting = (tl, intensity, hexColor, duration, label) => {
-                
-                //TODO: Set a default param for label in the function header
-                
-                let tween = TweenMax.to(
-                    bulbSVG,
-                    duration,
-                    {autoAlpha: intensity, fill: hexColor, ease: Power0.easeNone }  // TODO: Create a good easing function
-                );
-                
-                label = label || '+=0';
-                tl.add(tween, label);
-            },
-            
-            
-            increaseBulbLighting = (tl, intensity, hexColor, duration, label) => {              
-                setBulbLighting(tl, intensity, hexColor, duration, label);
-                tl.to(bulbBorderSVG, duration, {autoAlpha: intensity}, label);                
-            },
-            
 
-            attentuateLighting = (tl, intensity, hexColor, duration, label) => {
-                setBulbLighting(tl, intensity, hexColor, duration, label);
-                tl.to(bulbBorderSVG, duration, {autoAlpha: intensity}, label);
-            }
+            
+//            increaseBulbLighting = (tl, intensity, hexColor, duration, label) => {              
+//                setBulbLighting(tl, intensity, hexColor, duration, label);
+//            },
+//            
+//
+//            attentuateLighting = (tl, intensity, hexColor, duration, label) => {
+//                setBulbLighting(tl, intensity, hexColor, duration, label);
+//            },
             
             
             /**
@@ -116,32 +187,25 @@ let lightbulb = function lightbulb () {
 
                 wireChargeTL.to(
                     energyWireUnlitSVG,
-                    DURATIONS.wireCharging,
-                    { stroke: COLORS.wireCharging, ease: Power0.easeNone }
+                    DURATIONS.initialWireCharging,
+                    { stroke: COLORS.chargingOrange, ease: Power0.easeNone },
+                    LABELS.wireChargeStart
+                );
+                
+                wireChargeTL.set(bulbInnerChargingSVG, {scale: 1}, LABELS.wireChargeStart);
+                
+                setBulbLighting(
+                    wireChargeTL, 
+                    bulbInnerChargingSVG, 
+                    1, 
+                    COLORS.chargingOrange, 
+                    DURATIONS.initialWireCharging, 
+                    LABELS.wireChargeStart
                 );
 
                 return wireChargeTL;
             },
 
-            
-            /**
-             * While the wire is progressing to a full charge, 
-             * the bulb will faintly light up to reflect its color
-             * 
-             * (The glow, however, still remains invisible)
-             */
-            glimmerBulb = () => {
-
-                let bulbGlimmerTL = new TimelineMax ();
-
-                bulbGlimmerTL.to(
-                    bulbSVG,
-                    DURATIONS.wireCharging,
-                    { scale: 0, autoAlpha: 1}
-                );
-                
-                return bulbGlimmerTL;
-            },
 
             
             /**
@@ -154,23 +218,61 @@ let lightbulb = function lightbulb () {
             flickerToFullCharge = () => {
                 
                 let 
-                    flickerToFullTL = new TimelineMax(),                
+                    flickerToFullTL = new TimelineMax({delay: 2}),                
                     
-                    illuminateBulb = () => {                
+                    /**
+                     * Makes a timelime that coordinates a flickering of the bulb
+                     */
+                    makeFlickerInstance = (percentage, duration, label = getUniqueLabel()) => {                
                         
+                        let
+                            drawSVGValue = percentage + '% ' + (100-percentage) + '%',
+                            flickerInstanceTL = new TimelineMax();
+                        
+                        
+                        // scale down the inner charging layer and scale up the lit layer (in 
+                        // advance of animating the lit layer's opacity)
+                        
+                        // TODO: See if a short "to" wouldn't be better here
+                        flickerInstanceTL.set(bulbInnerChargingSVG, { autoAlpha: 0, scale: 0, immediateRender: false }); 
+                        flickerInstanceTL.set(bulbInnerLitSVG, { autoAlpha: 0, scale: 1, immediateRender: false });
+                        
+                        flickerInstanceTL.addLabel(label);
+                        
+                        flickerInstanceTL.fromTo(
+                            energyWireLitSVG, 
+                            duration, 
+                            { drawSVG: false },
+                            { drawSVG: drawSVGValue, ease: EASINGS.default },
+                            label
+                        );
+                                                
+                        setBulbLighting(
+                            flickerInstanceTL,
+                            bulbInnerLitSVG,
+                            percentage / 100,
+                            COLORS.litYellow,
+                            duration,
+                            label
+                        );
+                        
+                        return flickerInstanceTL;
                         
                     };
                 
+                flickerToFullTL.add(makeFlickerInstance(20, DURATIONS.chargeFlicker1, LABELS.chargeFlicker1), LABELS.chargeFlicker1Complete);
+                flickerToFullTL.add(makeFlickerInstance(40, DURATIONS.chargeFlicker2, LABELS.chargeFlicker2), '+2');
+                flickerToFullTL.add(makeFlickerInstance(60, DURATIONS.chargeFlicker3, LABELS.chargeFlicker3), '+5');
+                flickerToFullTL.add(makeFlickerInstance(70, DURATIONS.chargeFlicker4, LABELS.chargeFlicker4), '+7');
+                flickerToFullTL.add(makeFlickerInstance(80, DURATIONS.chargeFlicker5, LABELS.chargeFlicker5), '+9');
+                flickerToFullTL.add(makeFlickerInstance(99, DURATIONS.chargeFlicker6, LABELS.chargeFlicker6), '+10');                                
                 
-                return flickerToFullTL;
-                
+                return flickerToFullTL;                
             },
                                     
             
-            flickerBulbAtRandom = () => {
-              
-                let flickerTL = new TimelineMax({ repeat: -1 });
-                
+            flickerBulbAtRandom = () => {              
+                let flickerTL = new TimelineMax({ repeat: -1 });                
             },
 
 
@@ -183,15 +285,14 @@ let lightbulb = function lightbulb () {
                       
                 masterTL.add(setScene());
                 masterTL.add(chargeWire(), LABELS.phaseWireIsCharging);
-                masterTL.add(glimmerBulb(), LABELS.phaseWireIsCharging);
-                //masterTL.add(flickerToFullCharge(), LABELS.phaseFullChargeReached);
+                masterTL.add(flickerToFullCharge(), LABELS.phaseFlickeringToFullLight);
                 //masterTL.add(illuminateBulb(), LABELS.phaseFullChargeReached + '+=0.18');
                 //masterTL.add(flickerBulbAtRandom(), LABELS.phaseBulbOnAndFlickering);
-                
+                masterTL.play();
 
             };
 
-        masterTL = new TimelineMax();
+        masterTL = new TimelineMax({paused: true});
         letThereBeLight();
     };
         

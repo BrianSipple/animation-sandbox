b86a68f6340261e03f4c9c67980e4f919dd32b93
BrianSipple <Bsipple57@gmail.com>
2016-04-25 00:21:28 -0700

    cleaner icon

--- views/icons/toggle-animation/hamburger-menu-icon/toggle-to-x-circular-spin.js
+++ views/icons/toggle-animation/hamburger-menu-icon/toggle-to-x-circular-spin.js
@@ -8,25 +8,25 @@ const DURATIONS = {
 };
 
 const CLASS_NAMES = {
-  iconContainer: 'js-icon-container--toggle-to-x-circular-spin',
+  iconContainer: 'js-icon-root-svg--toggle-to-x-circular-spin',
   icon: 'icon',
   iconPaths: {
-    topLeftHamBottomRightX: 'icon-path icon-path--menu-top-x-tl-br',
+    menuTopLeftToXBottomRight: 'icon-path icon-path--menu-tl-to-x-br',
     menuMiddle: 'icon-path icon-path--menu-middle',
-    bottomRightHamTopRightX: 'icon-path icon-path--menu-bottom-x-bl-tr'
+    menuBottomRightToXTopRight: 'icon-path icon-path--menu-br-to-x-tr'
   },
   controlPoints: {
-    hamburger: {
-      topLeft: 'hamburger-cp hamburger-cp--top-left',
-      topRight: 'hamburger-cp hamburger-cp--top-right',
-      bottomLeft: 'hamburger-cp hamburger-cp--bottom-left',
-      bottomRight: 'hamburger-cp hamburger-cp--bottom-right'
+    menu: {
+      topLeft: 'menu-control-point menu-control-point--tl',
+      topRight: 'menu-control-point menu-control-point--tr',
+      bottomLeft: 'menu-control-point menu-control-point--bl',
+      bottomRight: 'menu-control-point menu-control-point--br'
     },
     x: {
-      topLeft: 'x-cp x-cp--top-left',
-      topRight: 'x-cp x-cp--top-right',
-      bottomLeft: 'x-cp x-cp--bottom-left',
-      bottomRight: 'x-cp x-cp--bottom-right'
+      topLeft: 'x-control-point x-control-point--tl',
+      topRight: 'x-control-point x-control-point--tr',
+      bottomLeft: 'x-control-point x-control-point--bl',
+      bottomRight: 'x-control-point x-control-point--br'
     }
   }
 };
@@ -47,16 +47,16 @@ const Icon = (function Icon() {
     DOM_REFS.iconContainer = iconContainer;
     DOM_REFS.icon = iconContainer.getElementsByClassName(CLASS_NAMES.icon)[0];
     DOM_REFS.iconPaths = {
-      topLeftHamBottomRightX: iconContainer.getElementsByClassName(CLASS_NAMES.iconPaths.topLeftHamBottomRightX)[0],   // menu top / the part of the "x" that starts (left-to-right) at the top-left
+      menuTopLeftToXBottomRight: iconContainer.getElementsByClassName(CLASS_NAMES.iconPaths.menuTopLeftToXBottomRight)[0],   // menu top / the part of the "x" that starts (left-to-right) at the top-left
       menuMiddle: iconContainer.getElementsByClassName(CLASS_NAMES.iconPaths.menuMiddle)[0],
-      bottomRightHamTopRightX: iconContainer.getElementsByClassName(CLASS_NAMES.iconPaths.bottomRightHamTopRightX)[0], // menu top / the part of the "x" that starts (left-to-right) at the bottom-left
+      menuBottomRightToXTopRight: iconContainer.getElementsByClassName(CLASS_NAMES.iconPaths.menuBottomRightToXTopRight)[0], // menu top / the part of the "x" that starts (left-to-right) at the bottom-left
     };
     DOM_REFS.controlPoints = {
-      hamburger: {
-        topLeft: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.hamburger.topLeft)[0],
-        topRight: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.hamburger.topRight)[0],
-        bottomLeft: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.hamburger.bottomLeft)[0],
-        bottomRight: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.hamburger.bottomRight)[0],
+      menu: {
+        topLeft: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.menu.topLeft)[0],
+        topRight: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.menu.topRight)[0],
+        bottomLeft: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.menu.bottomLeft)[0],
+        bottomRight: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.menu.bottomRight)[0],
       },
       x: {
         topLeft: iconContainer.getElementsByClassName(CLASS_NAMES.controlPoints.x.topLeft)[0],
@@ -91,7 +91,7 @@ const Icon = (function Icon() {
         masterTL.play(0);
 
       } else {
-        // Reverse back to the hamburger state.
+        // Reverse back to the menu state.
         // NOTE: 0 tell `reverse` to set the playhead at the end of the TL --
         // and and then wind back from there.
         masterTL.reverse(0);
@@ -114,10 +114,10 @@ const Icon = (function Icon() {
 
 
   function prepareStartState() {
-    const topLeftHamburgerControlPoint = DOM_REFS.controlPoints.hamburger.topLeft;
-    const topRightHamburgerControlPoint = DOM_REFS.controlPoints.hamburger.topRight;
-    const bottomLeftHamburgerControlPoint = DOM_REFS.controlPoints.hamburger.bottomLeft;
-    const bottomRightHamburgerControlPoint = DOM_REFS.controlPoints.hamburger.bottomRight;
+    const topLeftHamburgerControlPoint = DOM_REFS.controlPoints.menu.topLeft;
+    const topRightHamburgerControlPoint = DOM_REFS.controlPoints.menu.topRight;
+    const bottomLeftHamburgerControlPoint = DOM_REFS.controlPoints.menu.bottomLeft;
+    const bottomRightHamburgerControlPoint = DOM_REFS.controlPoints.menu.bottomRight;
 
     const topLeftHamburgerX = Number(topLeftHamburgerControlPoint.getAttribute('cx'));
     const topRightHamburgerX = Number(topRightHamburgerControlPoint.getAttribute('cx'));
@@ -126,36 +126,36 @@ const Icon = (function Icon() {
 
     const topHamburgerDrawPct = (
       Math.abs(topRightHamburgerX - topLeftHamburgerX) /
-      DOM_REFS.iconPaths.topLeftHamBottomRightX.getTotalLength()
+      DOM_REFS.iconPaths.menuTopLeftToXBottomRight.getTotalLength()
     ) * 100;
     const bottomHamburgerDrawPct = (
       Math.abs(bottomRightHamburgerX - bottomLeftHamburgerX) /
-      DOM_REFS.iconPaths.bottomRightHamTopRightX.getTotalLength()
+      DOM_REFS.iconPaths.menuBottomRightToXTopRight.getTotalLength()
     ) * 100;
 
     debugger;
     // TweenMax.set(
-    //   DOM_REFS.iconPaths.topLeftHamBottomRightX,
+    //   DOM_REFS.iconPaths.menuTopLeftToXBottomRight,
     //   {
     //     drawSVG: false,
     //   }
     // );
     //
     // TweenMax.set(
-    //   DOM_REFS.iconPaths.bottomRightHamTopRightX,
+    //   DOM_REFS.iconPaths.menuBottomRightToXTopRight,
     //   {
     //     drawSVG: '50%',
     //   }
     // );
     TweenMax.set(
-      DOM_REFS.iconPaths.topLeftHamBottomRightX,
+      DOM_REFS.iconPaths.menuTopLeftToXBottomRight,
       {
         drawSVG: `${100 - topHamburgerDrawPct}% 100%`,
       }
     );
 
     TweenMax.set(
-      DOM_REFS.iconPaths.bottomRightHamTopRightX,
+      DOM_REFS.iconPaths.menuBottomRightToXTopRight,
       {
         drawSVG: `100% ${100 - bottomHamburgerDrawPct}%`,
       }

221b26343d133199ac05dcad338ed2fe07d0924c
BrianSipple <Bsipple57@gmail.com>
2016-06-09 00:44:02 -0700

    initial artwork and setup for moon masking

--- views/moon-masking/main.js
+++ views/moon-masking/main.js
@@ -1,3 +1,39 @@
+const { assign, keys } = Object;
+
+const SELECTOR_MAP = {
+  singles: {
+    phaseSliderInputElem: '#moon-phase-slider-input',
+    phaseSliderOutputElem: '#moon-phase-slider-output',
+    earthShadowMaskElem: '#earth-shadow-mask',
+    moon: '#moon',
+  },
+  groups: {
+    backgroundStarElems: '.background-star'
+  }
+};
+
+const DOM_REFS = {};
+
+
+function cacheDOMRefs() {
+  keys(SELECTOR_MAP.singles).forEach(elemName => {
+    DOM_REFS[elemName] = document.querySelector(SELECTOR_MAP.singles[elemName]);
+  });
+  keys(SELECTOR_MAP.groups).forEach(elemName => {
+    DOM_REFS[elemName] = document.querySelectorAll(SELECTOR_MAP.groups[elemName]);
+  });
+}
+
+function onPhaseSliderInput(event) {
+  debugger;
+  const slidePercentage = parseFloat(event.target.value);
+  DOM_REFS.phaseSliderOutputElem.value = slidePercentage;
+}
+
+function addListeners() {
+  DOM_REFS.phaseSliderInputElem.addEventListener('input', onPhaseSliderInput, false);
+}
+
 function animateMask() {
 
 }
@@ -14,6 +50,8 @@ function initAnimation () {
 }
 
 function run() {
+  cacheDOMRefs();
+  addListeners();
   initAnimation();
 }
 
